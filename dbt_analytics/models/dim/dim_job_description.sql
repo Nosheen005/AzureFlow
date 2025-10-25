318896031d2ec435dc1b376e57cff03dfca93d6b
Nosheen005 <nosheen.yasheen05@gmail.com>
2025-10-25 11:26:20 +0200

    Changes to dim and fac job

--- dbt_analytics/models/dim/dim_job_description.sql
+++ dbt_analytics/models/dim/dim_job_description.sql
@@ -2,7 +2,8 @@ with src_job_description as (select * from {{ ref('src_job_description') }})
 
 select
     id as job_description_id,
-    headline,
-    description_text,
-    description_html
+    max (headline) as headline,
+    max (description_text) as description_text,
+    max (description_html) as description_html
 from src_job_description
+group by id
