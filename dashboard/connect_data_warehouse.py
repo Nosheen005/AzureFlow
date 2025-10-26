16d1974e534635d929e44b2766eac905e513a6c9
Anton_Hem <antonbackeus@gmail.com>
2025-10-26 22:27:55 +0100

    Adding dashboard functionality to azure patch

--- dashboard/connect_data_warehouse.py
+++ dashboard/connect_data_warehouse.py
@@ -1,7 +1,9 @@
 import duckdb
 import os
+from pathlib import Path
 
-DB_PATH = os.getenv("DUCKDB_PATH")
+DB_PATH = Path("/mnt/data/job_ads.duckdb")
+#DB_PATH = os.getenv("DUCKDB_PATH")
 #DB_PATH='../data_warehouse/job_ads.duckdb'
 
 def query_job_listings(query='SELECT * FROM marts.mart_construction'):
