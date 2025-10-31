e18d6f24815e71d942def2b8ce20bb1ed7c2bb57
AntonBackeus <antonbackeus@gmail.com>
2025-10-31 11:23:17 +0100

    remaining files

--- terraform/storage-account.tf
+++ terraform/storage-account.tf
@@ -5,18 +5,20 @@ resource "azurerm_storage_account" "my_storage" {
   account_tier             = var.storage_account_tier
   account_replication_type = var.storage_replication_type
 
-  tags = { environment = "staging" }
+  tags = {
+    environment = "staging"
+  }
 }
 
 resource "azurerm_storage_share" "my_share" {
   name                 = var.file_share_name
-  storage_account_id = azurerm_storage_account.my_storage.id
+  storage_account_name = azurerm_storage_account.my_storage.name
   quota                = var.file_share_size
 }
 
 resource "azurerm_storage_share_directory" "dbt_profiles_dir" {
-  name                 = ".dbt"
-  storage_share_id     = azurerm_storage_share.my_share.id
+  name             = ".dbt"
+  storage_share_id = azurerm_storage_share.my_share.id
 }
 
 resource "null_resource" "wait_for_fileshare" {
@@ -29,13 +31,13 @@ resource "null_resource" "wait_for_fileshare" {
 }
 
 resource "azurerm_storage_share_file" "dbt_profiles" {
-  name               = "profiles.yml"
-  source             = "${path.module}/profiles.yml"
-  storage_share_id   = azurerm_storage_share.my_share.id
-  path               = ".dbt"
-  depends_on         = [
+  name             = "profiles.yml"
+  source           = "${path.module}/profiles.yml"
+  storage_share_id = azurerm_storage_share.my_share.id
+  path             = ".dbt"
+
+  depends_on = [
     azurerm_storage_share_directory.dbt_profiles_dir,
     null_resource.wait_for_fileshare
   ]
 }
-
